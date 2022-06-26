0845aae3ca3cd0e58c15f86c39a931cda6cfc9eb
Issa Issa <mrforesightii@gmail.com>
2022-06-25 20:07:14 -0500

    second update

--- default-app/src/App.js
+++ default-app/src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
     super();
 
     this.state = {
-      name: 'Issa'
+      name: {firstname: 'Issa', lastname: 'Issa'},
+      company: 'The Bernard Group'
     }
   }
 
@@ -19,11 +20,17 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello {this.state.name}.
+            Hello {this.state.name.firstname} {this.state.name.lastname}. And I work with {this.state.company}
           </p>
           <button onClick={ () => {
-            this.setState({ name: 'Andrew' })
-          }}>Change Name</button>
+            this.setState(() => {
+              return {
+                name: {firstname: 'Andrew', lastname: 'Xiong'}
+              }
+            }, () => {})
+          }}>
+              Change Name
+            </button>
         </header>
       </div>
     );
